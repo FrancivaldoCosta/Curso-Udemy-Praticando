78847d0d4cb8c352806360afb3bee7673797e1b8
Francivaldo Costa <francivaldoaccosta@gmail.com>
2024-03-25 01:39:17 -0300

    Terminado 1

--- exercicios/src/util/Calculator.java
+++ exercicios/src/util/Calculator.java
@@ -2,14 +2,14 @@ package util;
 
 public class Calculator {
 
-	public final double PI = 3.14159;
+	public static final double PI = 3.14159;
 
-	public double circumference(double radius) {
+	public static double circumference(double radius) {
 		return 2.0 * PI * radius;
 
 	}
 
-	public double volume(Double radius) {
+	public static double volume(Double radius) {
 		return 4.0 * PI * radius * radius * radius / 3.0;
 
 	}
